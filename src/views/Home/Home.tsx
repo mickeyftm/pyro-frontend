ad9ed6871639d2809f9db0831007514dab77ca08
pyroswap <pyroswap@protonmail.com>
2021-05-26 21:17:11 +0200

    Update

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -13,15 +13,12 @@ import EarnAPRCard from 'views/Home/components/EarnAPRCard'
 
 const Hero = styled.div`
   align-items: center;
-  background-image: url('/images/pan-bg-mobile.svg');
-  background-repeat: no-repeat;
-  background-position: top center;
   display: flex;
   justify-content: center;
   flex-direction: column;
   margin: auto;
   margin-bottom: 32px;
-  padding-top: 116px;
+  padding-top: 15px;
   text-align: center;
 
   ${({ theme }) => theme.mediaQueries.lg} {
